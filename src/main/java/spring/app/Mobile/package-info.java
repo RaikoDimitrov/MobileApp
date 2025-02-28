e608404f4a764659bf05e3193bab9d90f54b150a
Raiko Dimitrov <98freddy98@gmail.com>
2025-02-28 17:09:39 +0200

    todo package

--- src/main/java/spring/app/Mobile/package-info.java
+++ src/main/java/spring/app/Mobile/package-info.java
@@ -3,10 +3,17 @@ package spring.app.Mobile;
 //todo:
 /*
 
-🚗 Search & Filters (Brand, Model, Year, Price Range, Location)
-🚗 Vehicle Comparison Tool
+search and filtration (Brand, Model, Year, Price Range, Location)
+vehicle comparison tool
 pageable for offers
 user features
+error/exception handling
+aop
+unit and integration tests
 jwt authentication ?
+web flux ?
+event - kafka ?
+microservice ?
+containerization and deployment
 
 */
